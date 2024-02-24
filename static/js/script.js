10ee2d75a56fcf94e23eb2a61ea1b38abbd2ac48
Jackie <zjackiewang03@gmail.com>
2024-02-24 23:25:16 +0000

    wheres the gaming room

--- static/js/script.js
+++ static/js/script.js
@@ -102,3 +102,14 @@ answerClicked(currentq.answer == currentq.options[1]);  });
   }
 }
 
+var audio = document.getElementById('audioPlayer');
+var playButton = document.getElementById('playButton');
+var pauseButton = document.getElementById('pauseButton');
+
+playButton.addEventListener('click', function() {
+    audio.play();
+});
+
+pauseButton.addEventListener('click', function() {
+    audio.pause();
+});
