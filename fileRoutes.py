ac51e14d014112eefc0cfd0e2b931c7b39bb50e3
Jackie <zjackiewang03@gmail.com>
2024-02-24 18:35:39 +0000

    1835

--- fileRoutes.py
+++ fileRoutes.py
@@ -1,5 +1,4 @@
 from flask import render_template, Blueprint, request
-from functions import generateRoomCode 
 import random
 
 routeManager = Blueprint('routeManager', __name__, template_folder='templates', static_folder='static')
@@ -36,32 +35,15 @@ def joinRoom():
 
 @routeManager.route('/rooms', methods=["POST", "GET"])
 def rooms():
-
     return render_template('rooms.html')
-
-@routeManager.route('/testing', methods=["POST", "GET"])
-def testing():
-    generateRoomCode()
-    return "Done"
-
 
 @routeManager.route('/getJson',methods=["POST","GET"])
 def getJson():
-
-    data = [{
-            "name":"roomName",
-            "codes": "XXXX"
-
-            },
-            {
-            "name":"roomName",
-            "codes": "XXXX"
-            },
-            {
-                "name":"roomName",
-                "codes": "XXXX"
-            }
-            
-            ]
-
-    return data
+    return {"hello":"parth"}
+                    
+@routeManager.route('/studyRoom', methods=["POST","GET"])
+def studyRoom():
+    roomName = request.form["roomName"]
+    roomCode = request.form["roomCode"]
+    return render_template('studyRoom.html',roomName = roomName, roomCode = roomCode)
+#[{codes:...,name:...}...]
