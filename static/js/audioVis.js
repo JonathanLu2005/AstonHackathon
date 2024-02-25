f280b8e1ec9d81d58aa8019306bb80a6cdce93e8
Ryan Valenzuela-Porta <ryanpogibags30@gmail.com>
2024-02-25 09:47:00 +0000

    fixed BIG ISSUE

--- static/js/audioVis.js
+++ static/js/audioVis.js
@@ -1,57 +1,57 @@
-var audio = document.getElementById('audioPlayer');
-var playButton = document.getElementById('playButton');
-var pauseButton = document.getElementById('pauseButton');
+// var audio = document.getElementById('audioPlayer');
+// var playButton = document.getElementById('playButton');
+// var pauseButton = document.getElementById('pauseButton');
 
-// Get the canvas element and its 2D rendering context
-const canvas = document.getElementById('visualizer');
-const ctx = canvas.getContext('2d');
+// // Get the canvas element and its 2D rendering context
+// //const canvas = document.getElementById('visualizer');
+// // const ctx = canvas.getContext('2d');
 
-// Set up audio context and analyzer
-const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
-const analyser = audioCtx.createAnalyser();
+// // Set up audio context and analyzer
+// const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+// const analyser = audioCtx.createAnalyser();
 
-// Function to draw visualizer
-function draw() {
-    requestAnimationFrame(draw);
+// // Function to draw visualizer
+// function draw() {
+//     requestAnimationFrame(draw);
 
-    // Copy frequency data to dataArray
-    analyser.getByteFrequencyData(dataArray);
+//     // Copy frequency data to dataArray
+//     analyser.getByteFrequencyData(dataArray);
 
-    // Clear canvas
-    ctx.fillStyle = 'black';
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
+//     // Clear canvas
+//     ctx.fillStyle = 'black';
+//     ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-    // Draw visualizer
-    const barWidth = (canvas.width / bufferLength) * 2.5;
-    let x = 0;
-    for(let i = 0; i < bufferLength; i++) {
-        const barHeight = dataArray[i] / 2;
-        const r = barHeight + (25 * (i/bufferLength));
-        const g = 250 * (i/bufferLength);
-        const b = 50;
-        ctx.fillStyle = 'rgb(' + r + ',' + g + ',' + b + ')';
-        ctx.fillRect(x, canvas.height - barHeight, barWidth, barHeight);
-        x += barWidth + 1;
-    }
-}
+//     // Draw visualizer
+//     const barWidth = (canvas.width / bufferLength) * 2.5;
+//     let x = 0;
+//     for(let i = 0; i < bufferLength; i++) {
+//         const barHeight = dataArray[i] / 2;
+//         const r = barHeight + (25 * (i/bufferLength));
+//         const g = 250 * (i/bufferLength);
+//         const b = 50;
+//         ctx.fillStyle = 'rgb(' + r + ',' + g + ',' + b + ')';
+//         ctx.fillRect(x, canvas.height - barHeight, barWidth, barHeight);
+//         x += barWidth + 1;
+//     }
+// }
 
-// Connect audio source to analyzer
-const audioSource = audioCtx.createMediaElementSource(audio);
-audioSource.connect(analyser);
-audioSource.connect(audioCtx.destination);
+// // Connect audio source to analyzer
+// const audioSource = audioCtx.createMediaElementSource(audio);
+// audioSource.connect(analyser);
+// audioSource.connect(audioCtx.destination);
 
-// Set up analyzer properties
-analyser.fftSize = 2048;
-const bufferLength = analyser.frequencyBinCount;
-const dataArray = new Uint8Array(bufferLength);
+// // Set up analyzer properties
+// analyser.fftSize = 2048;
+// const bufferLength = analyser.frequencyBinCount;
+// const dataArray = new Uint8Array(bufferLength);
 
-// Call draw function
-draw();
+// // Call draw function
+// draw();
 
-playButton.addEventListener('click', function() {
-    audio.play();
-});
+// playButton.addEventListener('click', function() {
+//     audio.play();
+// });
 
-pauseButton.addEventListener('click', function() {
-    audio.pause();
-});
+// pauseButton.addEventListener('click', function() {
+//     audio.pause();
+// });
