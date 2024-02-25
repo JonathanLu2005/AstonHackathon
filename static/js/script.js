65f1f7f5dd4cadff942b0f84debee68c126f0d85
Jackie <zjackiewang03@gmail.com>
2024-02-25 01:01:10 +0000

    nothing

--- static/js/script.js
+++ static/js/script.js
@@ -101,15 +101,3 @@ answerClicked(currentq.answer == currentq.options[1]);  });
     update();
   }
 }
-
-var audio = document.getElementById('audioPlayer');
-var playButton = document.getElementById('playButton');
-var pauseButton = document.getElementById('pauseButton');
-
-playButton.addEventListener('click', function() {
-    audio.play();
-});
-
-pauseButton.addEventListener('click', function() {
-    audio.pause();
-});
